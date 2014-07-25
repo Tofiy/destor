7b40a9860216fb67debad64e705588c4b225e54e
Tofiy <414970752@qq.com>
2014-07-25 10:26:42 +0800

    Update kvstore_mysql.h

--- src/index/kvstore_mysql.h
+++ src/index/kvstore_mysql.h
@@ -2,6 +2,7 @@
 #define KVSTORE_MYSQL_H_
 
 #define MAX_VALUE_NUM 4
+#define TEMPORARY_ID -1
 #define NUM_HASHES 4
 #define SETBIT(a, n) (a[n/CHAR_BIT] |= (1<<(n%CHAR_BIT)))
 #define GETBIT(a, n) (a[n/CHAR_BIT] & (1<<(n%CHAR_BIT)))
